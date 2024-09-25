534ba7aaede4b2910a4dde592a52cd64ec363a59
MahyarBHZ <mahyarbhz@gmail.com>
2024-09-25 23:14:43 +0330

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.systemmaestro"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework:spring-web")
+	// implementation("org.springframework:spring-boot-starter-tomcat")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/systemmaestro/api/SystemMaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemmaestro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemMaestroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
